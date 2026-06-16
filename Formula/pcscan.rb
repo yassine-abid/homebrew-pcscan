e3db45875139d47ba3a8d0b377afcaedfb3aa5c5
Abid Yassine <123458959+yassine-abid@users.noreply.github.com>
2026-06-16 22:17:41 +0100

    consolidate scanner + Homebrew formula into one repo

---
+++ Formula/pcscan.rb
@@ -0,0 +1,17 @@
+class Pcscan < Formula
+  desc "Read-only defensive PC health, security & investigation scanner"
+  homepage "https://github.com/yassine-abid/homebrew-pcscan"
+  url "https://github.com/yassine-abid/homebrew-pcscan/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "PLACEHOLDER"
+  version "1.1.0"
+
+  depends_on "clamav"
+
+  def install
+    bin.install "pcscan.sh" => "pcscan"
+  end
+
+  test do
+    assert_match "pcscan", shell_output("#{bin}/pcscan --help")
+  end
+end
